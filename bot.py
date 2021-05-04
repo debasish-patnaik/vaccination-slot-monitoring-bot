349d7140cb7df0a3677ad503409296b7b367a98a
Debasish Patnaik <imdebasishpatnaik@gmail.com>
2021-05-04 19:15:50 +0530

    chore: revert changes

--- bot.py
+++ bot.py
@@ -17,19 +17,12 @@ payload = {
     "date": date,
 }
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
-}
+response = requests.get(VACCINATION_SLOT_API_URL, params=payload).json()
 
-response = requests.get(
-    VACCINATION_SLOT_API_URL, params=payload, headers=headers
-)
-print(response)
-
-# if len(response["centers"]) > 0:
-#     message = "<b>Vaccination Slots are available, check COWIN API or website for details.</b>"
-#     print(
-#         requests.post(
-#             IFTTT_URL + os.getenv("ifttt_api_key"), data={"value1": message}
-#         ).content.decode("ascii")
-#     )
+if len(response["centers"]) > 0:
+    message = "<b>Vaccination Slots are available, check COWIN API or website for details.</b>"
+    print(
+        requests.post(
+            IFTTT_URL + os.getenv("ifttt_api_key"), data={"value1": message}
+        ).content.decode("ascii")
+    )
